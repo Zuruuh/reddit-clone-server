3b98e6fe275a136e244d192a137ccdd66fe69be4
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-23 20:00:55 +0200

    ✨ Created Basic Post Crud Resolver

---
+++ src/resolvers/post.ts
@@ -0,0 +1,58 @@
+import { Post } from "../Entities/Post";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
+import type { ResolverContext } from "../types";
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(@Ctx() { em }: ResolverContext): Promise<Post[]> {
+    return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg("id") id: number,
+    @Ctx() { em }: ResolverContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: ResolverContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title") title: string,
+    @Ctx() { em }: ResolverContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      post.title = title;
+    }
+
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: ResolverContext
+  ): Promise<Boolean> {
+    await em.nativeDelete(Post, { id }).catch(() => {
+      return false;
+    });
+    return true;
+  }
+}
