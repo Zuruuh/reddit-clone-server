89f4f31901ae8b0b02af447c0f21a9ae9ed033f7
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-23 14:20:04 +0200

    🥅 Catch errors in main method 🗃️ Migrates existing migrations on each run

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./Entities/Post";
 import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
-
-  const post = orm.em.create(Post, { title: "My first post :D" });
-  await orm.em.persistAndFlush(post);
+  await orm.getMigrator().up();
 };
 
-main();
+main().catch((err) => {
+  console.error(err);
+});
