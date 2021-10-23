1fa5a1b27cf078a1bb4e8ba880f79a2da04770b5
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-24 01:37:55 +0200

    🩹 Changed __prod__ constant behaviour

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const __prod__ = process.env.NODE_ENV !== "production";
+export const __prod__ = process.env.NODE_ENV === "production";
 export const __db_user__ = process.env.__db_user__ ?? "root";
 export const __db_password__ = process.env.__db_password__ ?? "root";
