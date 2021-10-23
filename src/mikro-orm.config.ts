813838654b36f7bba00e34a576044b8fa1dc41f6
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-23 14:18:55 +0200

    🔧 Created Mikro-Orm config

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,14 +1,19 @@
 import { MikroORM } from "@mikro-orm/core";
-import { join } from "path";
-import { __prod__ } from "./constants";
 import { Post } from "./Entities/Post";
 
-export default {
+const { join } = require("path");
+const { __db_user__, __db_password__, __prod__ } = require("./constants");
+
+const dbConfig = {
   type: "postgresql",
   dbName: "reddit-clone",
   debug: !__prod__,
-  user: "root",
-  password: "root",
+  user: __db_user__,
+  password: __db_password__,
+};
+
+export default {
+  ...dbConfig,
   migrations: {
     path: join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
