9eb7ce62b0ccb8fac848c036e0c8773a35286653
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-25 05:11:19 +0200

    🔧 Renamed types file & updated Session type

--- src/types.d.ts
+++
@@ -1,8 +0,0 @@
-import { EntityManager, Connection, IDatabaseDriver } from "@mikro-orm/core";
-import { Request, Response, Express } from "express";
-
-export type ResolverContext = {
-  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-  req: Request & { session: Express.session };
-  res: Response;
-};
