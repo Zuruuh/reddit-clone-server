547a7140d4bf8190cbcf4acd17105052f3869543
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-25 05:12:35 +0200

    ✨ Created resetPassword & ForgotPassword mutations

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,45 +1,98 @@
-import { User } from "../Entities/User";
-import {
-  Resolver,
-  InputType,
-  Field,
-  Mutation,
-  Arg,
-  Ctx,
-  ObjectType,
-  Query,
-} from "type-graphql";
+import { User } from "../entities/User";
+import { Resolver, Mutation, Arg, Ctx, Query } from "type-graphql";
 import { hash, verify } from "argon2";
 import type { ResolverContext } from "../types";
-import { COOKIE } from "../constants";
-
-@InputType()
-class UsernamePasswordInput {
-  @Field()
-  username: string;
-  @Field()
-  password: string;
-}
-
-@ObjectType()
-class FieldError {
-  @Field()
-  field: string;
-  @Field()
-  message: string;
-}
-
-@ObjectType()
-class UserResponse {
-  @Field(() => [FieldError], { nullable: true })
-  errors?: FieldError[];
-
-  @Field(() => User, { nullable: true })
-  user?: User;
-}
+import { COOKIE, FORGOT_PASSWORD_PREFIX } from "../constants";
+import { UsernamePasswordInput } from "./UsernamePasswordInput";
+import { validateRegister } from "../validators/validateRegister";
+import { UserResponse } from "../utils/UserResponse";
+import { sendEmail } from "../utils/sendEmail";
+import { v4 } from "uuid";
+import { passwordValidator } from "../validators/passwordValidator";
 
 @Resolver()
 export class UserResolver {
+  // ! Reset Password
+  @Mutation(() => UserResponse)
+  async resetPassword(
+    @Arg("token") token: string,
+    @Arg("password") password: string,
+    @Ctx() { redis, em, req }: ResolverContext
+  ): Promise<UserResponse> {
+    const passwordInvalid = passwordValidator(password);
+
+    if (!!passwordInvalid) {
+      return passwordInvalid;
+    }
+
+    const key = `${FORGOT_PASSWORD_PREFIX}${token}`;
+    const userId = await redis.get(key);
+    if (!userId) {
+      return {
+        errors: [
+          {
+            field: "token",
+            message: "Invalid token",
+          },
+        ],
+      };
+    }
+
+    const user = await em.findOne(User, { id: parseInt(userId) });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "token",
+            message: "User no longer exists",
+          },
+        ],
+      };
+    }
+
+    user.password = await hash(password);
+    em.persistAndFlush(user);
+    await redis.del(key);
+
+    // * Login user
+    req.session.userId = user.id;
+
+    return { user };
+  }
+  // ! Forgot Password
+  @Mutation(() => Boolean)
+  async forgotPassword(
+    @Arg("email") email: string,
+    @Ctx() { em, redis }: ResolverContext
+  ): Promise<Boolean> {
+    const user = await em.findOne(User, { email });
+    if (!user) {
+      // This email is not currently in use
+      return true;
+    }
+
+    const token = v4();
+    await redis.set(
+      `${FORGOT_PASSWORD_PREFIX}${token}`,
+      user.id,
+      "ex",
+      1000 * 60 * 60 * 2
+    );
+
+    const href = `http://localhost:3000/reset-password/${token}`;
+    await sendEmail(
+      email,
+      "Reddit - Reset your password",
+      `<p>Hello ${user.username},</p><br><br>
+       <p>You recently requested to reset your password.</p><br>
+       <p>If you are at the origin of this action, click 
+       <a href="${href}">here</a> to reset it.</p><br>
+       <p>Else, you can just ignore this message</p>`
+    );
+    return true;
+  }
+
   // ! My Profile
   @Query(() => User, { nullable: true })
   async me(@Ctx() { req, em }: ResolverContext): Promise<User | null> {
@@ -55,41 +108,19 @@ export class UserResolver {
   @Mutation(() => UserResponse)
   async register(
     @Arg("options", () => UsernamePasswordInput) options: UsernamePasswordInput,
-    @Ctx() { em, req }: ResolverContext
+    @Ctx() context: ResolverContext
   ): Promise<UserResponse> {
-    const existing = await em.findOne(User, { username: options.username });
-    if (existing) {
-      return {
-        errors: [
-          {
-            field: "username",
-            message: "This username is already in use",
-          },
-        ],
-      };
+    const invalid = await validateRegister(options, context);
+    if (invalid) {
+      return invalid;
     }
-    if (options.username.length < 3) {
-      return {
-        errors: [
-          {
-            field: "username",
-            message: "Username must be at least 3 characters",
-          },
-        ],
-      };
-    }
-    if (options.password.length < 4) {
-      return {
-        errors: [
-          {
-            field: "password",
-            message: "Password must be at least 4 characters",
-          },
-        ],
-      };
-    }
-    const password = await hash(options.password);
-    const user = em.create(User, { username: options.username, password });
+    const { req, em } = context;
+    const password = await await hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password,
+      email: options.email,
+    });
     await em.persistAndFlush(user);
 
     req.session.userId = user.id;
@@ -99,22 +130,30 @@ export class UserResolver {
   // ! Login
   @Mutation(() => UserResponse)
   async login(
-    @Arg("options") options: UsernamePasswordInput,
+    @Arg("usernameOrEmail") usernameOrEmail: string,
+    @Arg("password") password: string,
     @Ctx() { em, req }: ResolverContext
   ): Promise<UserResponse> {
-    const user = await em.findOne(User, { username: options.username });
+    const user = await em.findOne(
+      User,
+      usernameOrEmail.includes("@")
+        ? { email: usernameOrEmail }
+        : { username: usernameOrEmail }
+    );
     if (!user) {
       return {
         errors: [
           {
-            field: "username",
-            message: "That username doesn't exist.",
+            field: "usernameOrEmail",
+            message: `That ${
+              usernameOrEmail.includes("@") ? "email" : "username"
+            } does not exist.`,
           },
         ],
       };
     }
 
-    const valid = await verify(user.password, options.password);
+    const valid = await verify(user.password, password);
     if (!valid) {
       return {
         errors: [
